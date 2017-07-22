c189f751765902d302215f2f2db74b3c6f08f59c
Ivan Dugalic <idugalic@gmail.com>
2017-07-22 21:58:21 +0200

    Fixing the AWS Lambda missing class - handler

--- serverless-company-aws/src/main/java/com/idugalic/SampleApplication.java
+++ serverless-company-aws/src/main/java/com/idugalic/SampleApplication.java
@@ -1,5 +1,5 @@
 
-package com.idugalic.aws;
+package com.idugalic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- serverless-company-aws/src/main/java/com/idugalic/handler/UppercaseFunctionHandler.java
+++ serverless-company-aws/src/main/java/com/idugalic/handler/UppercaseFunctionHandler.java
@@ -1,4 +1,4 @@
-package com.idugalic.aws.handler;
+package com.idugalic.handler;
 
 import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
 
